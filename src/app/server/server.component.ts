9761840ceda6de8ad57e2ca34c8ef7ef3c140ab8
Greg Charles <gregcharles@javaranch.com>
2020-05-09 16:35:51 -0700

    Lecture 25: String interpolation

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -5,5 +5,17 @@ import {Component} from '@angular/core';
   templateUrl: './server.component.html'
 })
 export class ServerComponent {
+  getServerId(): number {
+    return this.getRandomInt(1000, 10000);
+  }
 
+  getServerStatus() {
+    return Math.random() < 0.25 ? 'offline' : 'online';
+  }
+
+  private getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min; // The maximum is exclusive and the minimum is inclusive
+  }
 }
